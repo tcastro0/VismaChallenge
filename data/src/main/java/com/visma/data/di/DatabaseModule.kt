2ab907fe71f5eacf98e733c40c311981d14c6103
Tiago Castro <tiago.castro@kenbi.de>
2025-04-05 19:33:06 +0100

    feat(photocapture): dao + DI Setup

--- data/src/main/java/com/visma/data/di/DatabaseModule.kt
+++ data/src/main/java/com/visma/data/di/DatabaseModule.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Room
 import com.visma.data.database.VismaDatabase
 import com.visma.data.features.expenses.models.daos.ExpenseDao
+import com.visma.data.features.photocapture.models.daos.PhotoExpenseDao
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -28,5 +29,10 @@ object DatabaseModule {
     @Provides
     fun provideExpenseDao(db: VismaDatabase): ExpenseDao {
         return db.expenseDao()
+    }
+
+    @Provides
+    fun providePhotoExpenseDao(db: VismaDatabase): PhotoExpenseDao {
+        return db.photoExpenseDao()
     }
 }
